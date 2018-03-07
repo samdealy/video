2d275a659e36d38fed46c265baccb7197f19f50f
Sam Dealy <sam.dealy@gmail.com>
2018-03-07 12:37:15 -0500

    Create more css styling for nav bar, color ribbon etc.

--- frontend/components/nav_bar/nav_bar.jsx
+++ frontend/components/nav_bar/nav_bar.jsx
@@ -14,12 +14,18 @@ class NavBar extends React.Component {
 
     return(
       <div className="nav-bar">
+        <div id="color-ribbon"></div>
         <Link to='/'><h2>Video</h2></Link>
         <LeftNavBar  loggedIn={loggedIn} />
         <ul className="right-nav-section">
           <li><input placeholder="Search for videos..."></input></li>
           <li>{userIcon}</li>
-          <li><Link to="/upload"><span>Upload</span></Link></li>
+          <li>
+            <Link to="/upload" id="upload-button">
+              <i className="fas fa-cloud-upload-alt"></i>
+              <span>Upload</span>
+            </Link>
+          </li>
         </ul>
       </div>
     );
