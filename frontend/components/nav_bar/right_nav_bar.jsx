8899be3017588bd1c5a270e64c1239284851239e
Sam Dealy <sam.dealy@gmail.com>
2018-03-09 21:54:24 -0500

    Submit initial drop-down work

--- frontend/components/nav_bar/right_nav_bar.jsx
+++ frontend/components/nav_bar/right_nav_bar.jsx
@@ -4,11 +4,7 @@ import { Link } from 'react-router-dom'
 export default ({ currentUser, loggedIn }) => {
   let userIcon;
   if (loggedIn) {
-
-    userIcon =
-      <div className="avatar-wrapper">
-        <img src={currentUser.image_url} alt="current-user-avatar"></img>
-      </div>;
+    userIcon = <img src={currentUser.image_url} alt="current-user-avatar"></img>
   }
 
   return(
@@ -17,7 +13,28 @@ export default ({ currentUser, loggedIn }) => {
         <input id="search-bar" placeholder="Search for videos..."></input>
         <i className="fas fa-search"></i>
       </li>
-      <li>{userIcon}</li>
+      <li className="avatar-li">
+        <div className="avatar-wrapper">
+          {userIcon}
+        </div>
+      </li>
+      <div className="icon-dropdown">
+        <div className="dropdown-user-info-wrapper">
+          <div className="dropdown-user-info">
+          {userIcon}
+          <div className="view-profile-wrapper">
+            <h4>{currentUser.username}</h4>
+            <span>View profile</span>
+          </div>
+        </div>
+        </div>
+        <ul>
+          <li>
+            <button>Log out</button>
+          </li>
+        </ul>
+      </div>
+
       <li className="upload-button">
         <Link to="/upload" id="upload-button-link">
           <i className="fas fa-cloud-upload-alt"></i>
