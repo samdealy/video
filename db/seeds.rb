4bec087dc1661ac8180887b9c8592236454c220f
Sam Dealy <sam.dealy@gmail.com>
2018-03-12 09:10:58 -0400

    Make some video seeds that work

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,13 +17,23 @@ users = User.create([
   ])
 
 video_url_1 = "https://s3.amazonaws.com/fsp-video-dev/videos/video_seeds/Boys+Latin_1_4.mp4"
-video_url_2 = "https://s3.amazonaws.com/fsp-video-dev/videos/video_seeds/Boys+Latin+(2%3A4).mp4"
-video_url_3 = "https://s3.amazonaws.com/fsp-video-dev/videos/video_seeds/Boys+Latin+(3%3A4).mp4"
-video_url_4 = "https://s3.amazonaws.com/fsp-video-dev/videos/video_seeds/Boys+Latin+(4%3A4).mp4"
+video_url_2 = "https://s3.amazonaws.com/fsp-video-dev/videos/video_seeds/Boys+Latin_2_4.mp4"
+video_url_3 = "https://s3.amazonaws.com/fsp-video-dev/videos/video_seeds/Boys+Latin_3_4.mp4"
+video_url_4 = "https://s3.amazonaws.com/fsp-video-dev/videos/video_seeds/Boys+Latin_4_4.mp4"
+video_url_5 = "https://s3.amazonaws.com/fsp-video-dev/videos/video_seeds/Checking+out+sights.mp4"
+video_url_6 = "https://s3.amazonaws.com/fsp-video-dev/videos/video_seeds/C+E+R+A+M+I+C+S.mp4"
+video_url_7 = "https://s3.amazonaws.com/fsp-video-dev/videos/video_seeds/City+of+ink.mp4"
+video_url_8 = "https://s3.amazonaws.com/fsp-video-dev/videos/video_seeds/Ink+is+Incredible.mp4"
+video_url_9 = "https://s3.amazonaws.com/fsp-video-dev/videos/video_seeds/THIS.mp4"
 
 Video.create([
     { title: "Boys Latin (1/4)", uploader_id: User.first.id, clip: video_url_1 },
-    # { title: "Boys Latin (2/4)", uploader_id: User.first.id, clip: video_url_2 },
-    # { title: "Boys Latin (3/4)", uploader_id: User.first.id, clip: video_url_3 },
-    # { title: "Boys Latin (4/4)", uploader_id: User.first.id, clip: video_url_4 }
+    { title: "Boys Latin (2/4)", uploader_id: User.first.id, clip: video_url_2 },
+    { title: "Boys Latin (3/4)", uploader_id: User.first.id, clip: video_url_3 },
+    { title: "Boys Latin (4/4)", uploader_id: User.first.id, clip: video_url_4 },
+    { title: "Checking out sites", uploader_id: User.first.id, clip: video_url_5 },
+    { title: "C E R A M I C S", uploader_id: User.second.id, clip: video_url_6 },
+    { title: "City of Ink", uploader_id: User.second.id, clip: video_url_7 },
+    { title: "Ink is Incredible", uploader_id: User.second.id, clip: video_url_8 },
+    { title: "THIS", uploader_id: User.second.id, clip: video_url_9 }
   ])
