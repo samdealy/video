6e31bbc334e3632ff0d6a3af6baeceb993ef04b9
Sam Dealy <sam.dealy@gmail.com>
2018-03-13 14:29:45 -0400

    First draft of followings backend

---
+++ app/controllers/api/followings_controller.rb
@@ -0,0 +1,38 @@
+class Api::FollowingsController < ApplicationController
+  before_action :require_logged_in
+
+  def create
+    following = Following.new(following_params)
+    following.follower_id = current_user.id
+    if following.save!
+      @users   = [following.follower, following.leader]
+      render 'api/users/index'
+    else
+      render json: following.errors.full_messages, status: 422
+    end
+  end
+
+  def destroy
+    following = Following.find(params[:id])
+    if following_is_destroyable?(following)
+      following.destroy
+      @users = [following.follower, following.leader]
+      render 'api/users/index'
+    elsif following && !(following_is_destroyable?(following))
+      render json: {error: ["Can't edit another users follows"]}, status: 403
+    else
+      render json: {error: ["Can't find follower / followed user"]}, status: 404
+    end
+  end
+
+  private
+  def following_params
+    params.require(:following).permit(:leader_id)
+  end
+
+  def following_is_destroyable?(following)
+    follower_id, leader_id = following.follower_id, following.leader_id
+    following && (follower_id == current_user.id || leader_id == current_user.id)
+  end
+
+end
--- app/views/api/users/_user.json.jbuilder
+++ app/views/api/users/_user.json.jbuilder
@@ -1,3 +1,6 @@
 json.extract! user, :id, :username, :email
 json.image_url asset_path(user.image.url)
 json.video_ids user.videos.map{|vid| vid.id}
+
+json.follower_ids user.followers.map{ |follower| follower.id }
+json.leader_ids user.leaders.map{ |leader| leader.id }
