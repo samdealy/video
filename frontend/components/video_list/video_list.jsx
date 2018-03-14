251d39608eeec50631984250aa5dacad4b29bee2
Sam Dealy <sam.dealy@gmail.com>
2018-03-14 17:17:54 -0400

    Get load button text to change depending on whether or not there are more videos to be loaded

--- frontend/components/video_list/video_list.jsx
+++ frontend/components/video_list/video_list.jsx
@@ -9,32 +9,62 @@ class VideoList extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      numberOfRequests: 1
+      numberOfRequests: 1,
+      noMoreVideosText: false
     };
+    this.handleLoadMore = this.handleLoadMore.bind(this);
   }
 
   componentDidMount() {
     const { type, action } = this.props;
     if (type === "feed") {
-      action(this.state.numberOfRequests);
-      this.setState({ numberOfRequests: this.state.numberOfRequests + 1});
+      this.handleLoadMore();
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    debugger
+    if (this.props.videos.length === nextProps.videos.length ) {
+      this.setState({ noMoreVideosText: true });
     }
   }
 
   createVideoListItems() {
     const { videos, type } = this.props;
-    
     return videos.map( (video, i) => {
       return <VideoListItem type={type} video={video} key={i} />;
     });
   }
 
+  handleLoadMore(e) {
+    const { action } = this.props;
+    action(this.state.numberOfRequests);
+    this.setState({ numberOfRequests: this.state.numberOfRequests + 1});
+  }
+
+  createLoadButton() {
+    const { videos, type } = this.props;
+    let innerText;
+    if (this.state.noMoreVideosText) {
+      innerText = "THAT'S ALL FOLKS!";
+    } else {
+      innerText = "+ Load more videos"  ;
+    }
+    return(
+      <button className="load-more-button"
+        onClick={this.handleLoadMore}>{innerText}</button>
+    );
+  }
+
+
   render() {
     const { videos, type } = this.props;
     const videoListItems = this.createVideoListItems();
+    const loadMoreButton = type === "feed" ? this.createLoadButton() : '';
     return(
       <ul className="video-list">
         {videoListItems}
+        {loadMoreButton}
       </ul>
     );
   }
