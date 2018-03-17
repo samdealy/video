97c4c96ae15b774de6e8a5fcdc37830697d83144
Sam Dealy <sam.dealy@gmail.com>
2018-03-17 13:44:02 -0400

    Fix the thumbnail issue by removing the time parameter from the :thumb style on the clip attachment in the Video model.

--- app/models/video.rb
+++ app/models/video.rb
@@ -4,13 +4,10 @@ class Video < ApplicationRecord
 
   belongs_to :uploader, class_name: :User
   has_many :comments, dependent: :destroy
-  
+
   has_attached_file :clip, styles: {
-    :medium => {
-      :geometry => "640x480",
-      :format => 'mp4'
-    },
-    :thumb => { :geometry => "300x170", :format => 'jpeg', :time => 10}
+    medium: { geometry: "640x480", format: 'mp4'  },
+    thumb:  { geometry: "300x170", format: 'jpeg' },
     }, :processors => [:transcoder]
   validates_attachment_content_type :clip, content_type: /\Avideo\/.*\Z/
 
