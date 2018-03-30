b646550aa6ba6564784cb15a7a45822a0b9eb969
Sam <sam.dealy@gmail.com>
2018-03-30 11:11:30 -0400

    Add about_me

---
+++ frontend/components/home/about_me.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default () => {
+  return(
+    <div className="aside-section-wrapper">
+      <div className="aside-header about-me-header">About me</div>
+      <p className="about-me-text">
+        I'm Sam Dealy, a software engineer based in New York City. I love building
+        projects with Ruby on Rails, React-Redux, and tons of CSS.
+        <br></br>
+        <br></br>
+        I'm currently looking for full time front end and full stack developer roles.
+        See below for contact information!
+      </p>
+    </div>
+  )
+}
