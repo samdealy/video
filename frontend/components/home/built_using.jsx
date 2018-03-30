5cc8e51d42aad056eb894af6a88c130f3d09443b
Sam <sam.dealy@gmail.com>
2018-03-30 15:47:25 -0400

    Style skills logos

--- frontend/components/home/built_using.jsx
+++ frontend/components/home/built_using.jsx
@@ -4,7 +4,7 @@ import SkillsLogoList from '../logos/skills_logo_list';
 export default () => {
   return(
     <div className="aside-section-wrapper">
-      <div className="aside-header about-me">Built using</div>
+      <div className="aside-header built-using">Built using</div>
       <SkillsLogoList />
     </div>
   )
